dc38ee93d621de7e59b24cc05031b71549bcf935
jaelghiso <jael.ghiso@gmail.com>
2020-04-20 01:17:54 -0300

    adding reusables component and refactor drag & drop

--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -1,23 +1,30 @@
 <template>
-  <div
-    class="task"
-    draggable
-    @dragstart="pickupTask($event, taskIndex, columnIndex)"
-    @click="goToTask(task)"
-    @dragover.prevent
-    @dragenter.prevent
-    @drop.stop="moveTaskOrColumn($event, column.tasks, taskIndex, columnIndex)"
-  >
-    <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
-    <p class="w-full flex-no-shrink mt-1 text-sm" v-if="task.description">
-      {{ task.description }}
-    </p>
-  </div>
+  <AppDrop @drop="moveTaskOrColumn">
+    <AppDrag
+      class="task"
+      :transferData="{
+        type: 'task',
+        fromColumnIndex: columnIndex,
+        fromTaskIndex: taskIndex,
+      }"
+      @click="goToTask(task)"
+    >
+      <span class="w-full flex-no-shrink font-bold">
+        {{ task.name }}
+      </span>
+      <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">
+        {{ task.description }}
+      </p>
+    </AppDrag>
+  </AppDrop>
 </template>
-<script>
-import movingTasksOrColumnMixin from "../mixins/movingTasksOrColumnMixin";
 
+<script>
+import movingTasksOrColumnMixin from "@/mixins/movingTasksOrColumnMixin";
+import AppDrag from "./AppDrag";
+import AppDrop from "./AppDrop";
 export default {
+  components: { AppDrag, AppDrop },
   mixins: [movingTasksOrColumnMixin],
   props: {
     task: {
@@ -33,16 +40,10 @@ export default {
     goToTask(task) {
       this.$router.push({ name: "task", params: { id: task.id } });
     },
-    pickupTask(e, fromTaskIndex, fromColumnIndex) {
-      e.dataTransfer.effectAllowed = "move";
-      e.dataTransfer.dropEffect = "move";
-      e.dataTransfer.setData("from-task-index", fromTaskIndex);
-      e.dataTransfer.setData("from-column-index", fromColumnIndex);
-      e.dataTransfer.setData("type", "task");
-    },
   },
 };
 </script>
+
 <style lang="css">
 .task {
   @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -60,9 +60,4 @@ export default {
 .board {
   @apply p-4 bg-teal-dark h-full overflow-auto;
 }
-
-.task-bg {
-  @apply pin absolute;
-  background: rgba(0, 0, 0, 0.5);
-}
 </style>
